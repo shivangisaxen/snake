6db42a6f9e0589f2ae5edb04db65bd72d6a6adbf
shivangisaxen <107035866+shivangisaxen@users.noreply.github.com>
2022-06-23 15:51:41 +0530

    Add files via upload
    tt.js

---
+++ java.js
@@ -0,0 +1,120 @@
+let inputDir={x:0,y:0};
+let speed=2;
+let score=0;
+let lastPaintTime=0;
+let snakeArr=[
+    {x:13,y:15 }
+]
+
+food={x:6,y:7 };
+function main(ctime){
+    window.requestAnimationFrame(main);
+    console.log(ctime);
+    if((ctime-lastPaintTime)/1000<1/speed){
+  return;
+}
+lastPaintTime=ctime;
+gameEngine();
+}
+function isCollide(sarr){
+    return false;
+}
+
+function gameEngine(){
+    if(isCollide(snakeArr)){
+      inputDir={x:0,y:0};
+      alert("game over.press any key to pay a game")  
+      snakeArr=[{x:13,y:15}];
+      score=0;
+    }
+    if(snakeArr[0].y===food.y && snakeArr[0].x===food.x){
+        snakeArr.unshift({x:snakeArr[0].x+inputDir.x,y:snakeArr[0].y+inputDir.y})
+        let a=2;
+        let b=16;
+        food={x:Math.round(a+(b-a)*Math.random()),y:Math.round(a+(b-a)*Math.random())}
+    }
+    for(let i=snakeArr.length-2;i>=0;i--)
+        {
+            const element=array[i];
+            snakeArr[i+1]={...snakeArr[i]};
+        }
+        snakeArr[0].x+=inputDir.x;
+        snakeArr[0].y+=inputDir.y;
+    }
+
+board.innerHTML="";
+snakeArr.forEach((e,index)=>{
+snakeElement=document.createElement('div');
+snakeElement.style.gridRowStart=e.y;
+snakeElement.style.gridColumnStart=e.x;
+
+if(index===0){
+snakeElement.classList.add('head');
+}
+else{
+snakeElement.classList.add('snake');
+}
+board.appendChild(snakeElement);
+});
+//display the food element
+foodElement=document.createElement('div');
+foodElement.style.gridRowStart=food.y;
+foodElement.style.gridColumnStart=food.x;
+foodElement.classList.add('food');
+board.appendChild(foodElement);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//main logic starts here
+
+window.requestAnimationFrame(main);
+window.addEventListener('keydown',e=>{
+    inputDir={x:0,y:1}//start the game
+    switch(e.key){
+        case "ArrowUp":
+            console.log("ArrowUp")
+            inputDir.x=0;
+            inputDir.y=-1;
+            break;
+            case "ArrowDown":
+            console.log("ArrowDown")
+            inputDir.x=0;
+            inputDir.y=1;
+            break;
+            break;
+            case "ArrowLeft":
+            console.log("ArrowLeft")
+            inputDir.x=-1;
+            inputDir.y=0;
+            break;
+            break;
+            case "ArrowRight":
+            console.log("Arrowright")
+            inputDir.x=1;
+            inputDir.y=0;
+            break;
+            break;
+            default:
+                break;
+    }
+
+});
